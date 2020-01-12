ae47c56d7b9133eb93dd7d7171f9a0f32d1ac189
jirijancik <jirijancik2@gmail.com>
2020-01-13 00:48:27 +0100

    Router-GithubPages-Fix

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 /**
  * App showing data from themoviedb.org via API
- * Contains: Carousell, Movie Tiles, Movie Info Windows, Shaka Player
+ * Contains: Carousell, Movie Tiles, Movie Info Windows, Shaka Player, SearchPage
  * Technology: Mainly are used Funcional Components with hooks and for Movie Tiles is 
  * set up Movie Tile Context providin ID of current active Movie Tile.
+ * For Route there is used "process.env.PUBLIC_URL" so it works in developpement as well as in GitHub-Pages
  */
 function App() {
   return (   
@@ -20,8 +21,8 @@ function App() {
       <Switch>
         <MovieTileProvider key={"MovieTileProvider"}>
         
-          <Route path="/" exact component={MainPage} />
-          <Route path="/search" component={SearchPage} />
+          <Route path={process.env.PUBLIC_URL + '/'} exact component={MainPage} />
+          <Route path={process.env.PUBLIC_URL + '/search'} component={SearchPage} />
 
         </MovieTileProvider>
       </Switch>
