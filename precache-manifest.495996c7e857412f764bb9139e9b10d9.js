48992aff7f3873bde126c175bc02b1bdd62157f2
jirijancik <jirijancik2@gmail.com>
2020-01-12 23:49:56 +0100

    Updates

---
+++ precache-manifest.495996c7e857412f764bb9139e9b10d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6b9be65899cd3ab5d64bfd982e8245",
+    "url": "/react-movie-db/index.html"
+  },
+  {
+    "revision": "154417648f59922335c4",
+    "url": "/react-movie-db/static/css/2.b8bfb327.chunk.css"
+  },
+  {
+    "revision": "c4d36ac411c2fc0d0c07",
+    "url": "/react-movie-db/static/css/main.a9037eab.chunk.css"
+  },
+  {
+    "revision": "154417648f59922335c4",
+    "url": "/react-movie-db/static/js/2.942a480e.chunk.js"
+  },
+  {
+    "revision": "5f4393cb8a78b3f1b509ca5c400a26d1",
+    "url": "/react-movie-db/static/js/2.942a480e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4d36ac411c2fc0d0c07",
+    "url": "/react-movie-db/static/js/main.2bbad097.chunk.js"
+  },
+  {
+    "revision": "0895bd7968d828f567b3",
+    "url": "/react-movie-db/static/js/runtime-main.6cd280ca.js"
+  },
+  {
+    "revision": "70a7c7f30866faa8e8be1f2723459a2e",
+    "url": "/react-movie-db/static/media/ImageNotFound.70a7c7f3.jpg"
+  },
+  {
+    "revision": "7b34a834ceb67b44df594b3668572e16",
+    "url": "/react-movie-db/static/media/Stellar-light.7b34a834.otf"
+  }
+]);
